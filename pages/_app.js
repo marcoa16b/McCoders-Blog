780b4ff5a7b05d5acf953a8ad7a17e4c6cc85dd8
marcoa16b <62965695+marcoa16b@users.noreply.github.com>
2022-05-11 11:49:39 -0600

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,42 @@
 import '../styles/globals.css'
 import '../styles/markdown.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import CookieConsent from "react-cookie-consent";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+      const handleRouteChange = (url) => {
+          window.gtag("config", process.env.GOOGLE_ANALYTICS, {
+              page_path: url,
+          });
+      };
+      router.events.on("routeChangeComplete", handleRouteChange);
+      return () => {
+          router.events.off("routeChangeComplete", handleRouteChange);
+      };
+  }, [router.events]);
+  return (
+    <>
+      <Component {...pageProps} />
+      <CookieConsent
+        location="bottom"
+        buttonText="Sí, utilizar cookies."
+        onAccept={() => location.reload()}
+        cookieName="CookieConsent"
+        expires={150}
+        enableDeclineButton="true"
+        declineButtonText="No, no utilizar cookies"
+      >
+        Poner aquí el mensaje sobre el uso de cookies
+        <a href="#">Política de Cookies</a>.
+      </CookieConsent>
+    </>
+    
+  
+  )
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,39 @@ export default function Document() {
   return (
     <Html lang='es'>
       <Head>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/katex.min.css" integrity="sha384-KiWOvVjnN8qwAZbuQyWDIbfCLFhLXNETzBQjA/92pIowpC0d2O3nppDGQVgwd2nB" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/katex.min.css" integrity="sha384-KiWOvVjnN8qwAZbuQyWDIbfCLFhLXNETzBQjA/92pIowpC0d2O3nppDGQVgwd2nB" crossOrigin="anonymous" />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`} />
+        <script dangerouslySetInnerHTML={{
+            __html:`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              //this defaults to denying
+              gtag('consent', 'default', {
+                  'analytics_storage': 'denied'
+              });
+      
+              gtag('js', new Date());
+      
+              //este función es la que nos devuelve el valor de la cookie de preferencias
+              function getCookie() {
+                  const value = "; " + document.cookie;
+                  const parts = value.split("; CookieConsent=");
+                  if (parts.length === 2) return parts.pop().split(';').shift();
+              }
+      
+              //únicamente si el valor es true, se cargan los scripts de Google Analytics.
+              if(getCookie() === "true"){
+                  gtag('consent', 'update', {
+                      'analytics_storage': 'granted'
+                  });
+              }
+      
+              gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+              });
+            `,
+                }} 
+        />
       </Head>
       <body>
         <Main />
